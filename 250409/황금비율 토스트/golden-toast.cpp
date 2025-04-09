eedbef6f44a2ae62351a7e108242e5b1ae840282
Seo <sjw040108@gmail.com>
2025-04-09 13:04:09 +0900

    Title: 황금비율 토스트, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250409/황금비율 토스트/golden-toast.cpp
+++ 250409/황금비율 토스트/golden-toast.cpp
@@ -8,63 +8,47 @@ using namespace std;
 int n, m;
 string s;
 
+void printList(list<char> l) {
+
+    for (list<char>::iterator it = l.begin(); it != l.end(); it++) {
+        cout << *it;
+    }
+    cout<<endl;
+}
+
 int main() {
     cin >> n >> m;
     cin >> s;
 
     list<char>::iterator it;
-    list<char> v;
-
-    for (int i = 0; i < s.length(); i++) {
-        v.push_back(' ');
-        v.push_back(s[i]);
-    }
+    list<char> v(s.begin(), s.end());
 
     it = v.end();
 
     for (int i = 0; i < m; i++) {
         char command;
         cin >> command;
-        if (command == 'P') {
-            char c;
-            cin >> c;
 
-            it = v.insert(it, c);   // 문자 c 추가
-            it = v.insert(it, ' '); // 공백 추가
-            
-            ++it;
-        }
-        else if (command == 'L') {
-            if (it != v.begin()) {
-                --it;
-                --it;
-            }
+        if (command == 'L') {
+            if (it != v.begin()) --it;
         }
         else if (command == 'R') {
-            if (it != v.end()) {
-                ++it;
-                ++it;
-            }
+            if (it != v.end()) ++it;
         }
         else if (command == 'D') {
-            auto next = it;
-            if (next != v.end()) {
-                ++next;
-                if (next != v.end()) {
-                    next = v.erase(next);     // 문자 삭제
-                    if (next != v.end()) {
-                        next = v.erase(next); // 공백 삭제
-                    }
-                }
+            
+            if (it != v.end()) {
+                it = v.erase(it);           
             }
         }
-    }
-    for (list<char>::iterator iter = v.begin(); iter != v.end(); ++iter) {
-
-        if (*iter != ' ') {
-            cout << *iter;
+        else if (command == 'P') {
+            char c;
+            cin >> c;
+            v.insert(it, c);
         }
     }
+    for (list<char>::iterator iter = v.begin(); iter != v.end(); ++iter) { cout << *iter; }
 
     return 0;
 }
+    
